394a46ace7541697361d8cbe389de3961698a9f1
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-07-07 17:39:48 +0000

    Second commit

---
+++ second.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cin>>name;
+    cin>>age;
+
+    cout<<name<<endl;
+    cout<<age;
+
+    for(int i=1;i<=5;i++)
+    cout<<i<<" "<<endl;
+
+}
