c6babdd267d1fd5dce2cc35a7e94fc68a447c772
Priyanshu Gupta <priyanshusgbm05@gmail.com>
2026-07-07 17:53:27 +0000

    Third Commit

---
+++ array.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5]={10,20,30,40,50};
+
+    cout<<arr[0]<<endl;
+    cout<<arr[2]<<endl;
+
+
+    int brr[5];
+
+    for(int i=0;i<5;i++)
+    {
+         cin>> brr[i];
+    }
+
+    
+
+for(int i=0;i<5;i++)
+{
+    cout<< brr[i] << endl;
+}
+}
+
